c7645e10dadfa3e728591d6cfac8c58c6b51460b
nvcleemp <nico.vancleemput@gmail.com>
2013-03-11 21:11:39 +0100

    New program to count number of planar graphs in a file

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-all: statsPlanar
+all: statsPlanar countPlanar
 
 clean:
 
 statsPlanar: statsPlanar.c
 	cc -o statsPlanar -O4 statsPlanar.c
+
+countPlanar: countPlanar.c
+	cc -o countPlanar -O4 countPlanar.c
---
+++ countPlanar.c
@@ -0,0 +1,194 @@
+/* This program reads planar graphs from standard in and
+ * generates a summary of statistics for each graph.   
+ * 
+ * 
+ * Compile with:
+ *     
+ *     cc -o statsPlanar -O4 statsPlanar.c
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+
+#ifndef MAXN
+#define MAXN 64            /* the maximum number of vertices */
+#endif
+#define MAXE (6*MAXN-12)    /* the maximum number of oriented edges */
+#define MAXF (2*MAXN-4)      /* the maximum number of faces */
+#define MAXVAL (MAXN-1)  /* the maximum degree of a vertex */
+#define MAXCODELENGTH (MAXN+MAXE+3)
+
+#define INFI (MAXN + 1)
+
+int numberOfGraphs = 0;
+
+//=============== Reading and decoding planarcode ===========================
+
+/**
+ * 
+ * @param code
+ * @param laenge
+ * @param file
+ * @return returns 1 if a code was read and 0 otherwise. Exits in case of error.
+ */
+int readPlanarCode(unsigned short code[], int *length, FILE *file) {
+    static int first = 1;
+    unsigned char c;
+    char testheader[20];
+    int bufferSize, zeroCounter;
+    
+    int readCount;
+
+
+    if (first) {
+        first = 0;
+
+        if (fread(&testheader, sizeof (unsigned char), 15, file) != 15) {
+            fprintf(stderr, "can't read header ((1)file too small)-- exiting\n");
+            exit(1);
+        }
+        testheader[15] = 0;
+        if (strcmp(testheader, ">>planar_code<<") == 0) {
+
+        } else {
+            fprintf(stderr, "No planarcode header detected -- exiting!\n");
+            exit(1);
+        }
+    }
+
+    /* possibly removing interior headers -- only done for planarcode */
+    if (fread(&c, sizeof (unsigned char), 1, file) == 0) {
+        //nothing left in file
+        return (0);
+    }
+
+    if (c == '>') {
+        // could be a header, or maybe just a 62 (which is also possible for unsigned char
+        code[0] = c;
+        bufferSize = 1;
+        zeroCounter = 0;
+        code[1] = (unsigned short) getc(file);
+        if (code[1] == 0) zeroCounter++;
+        code[2] = (unsigned short) getc(file);
+        if (code[2] == 0) zeroCounter++;
+        bufferSize = 3;
+        // 3 characters were read and stored in buffer
+        if ((code[1] == '>') && (code[2] == 'p')) /*we are sure that we're dealing with a header*/ {
+            while ((c = getc(file)) != '<');
+            /* read 2 more characters: */
+            c = getc(file);
+            if (c != '<') {
+                fprintf(stderr, "Problems with header -- single '<'\n");
+                exit(1);
+            }
+            if (!fread(&c, sizeof (unsigned char), 1, file)) {
+                //nothing left in file
+                return (0);
+            }
+            bufferSize = 1;
+            zeroCounter = 0;
+        }
+    } else {
+        //no header present
+        bufferSize = 1;
+        zeroCounter = 0;
+    }
+
+    if (c != 0) /* unsigned chars would be sufficient */ {
+        code[0] = c;
+        if (code[0] > MAXN) {
+            fprintf(stderr, "Constant N too small %d > %d \n", code[0], MAXN);
+            exit(1);
+        }
+        while (zeroCounter < code[0]) {
+            code[bufferSize] = (unsigned short) getc(file);
+            if (code[bufferSize] == 0) zeroCounter++;
+            bufferSize++;
+        }
+    } else {
+        readCount = fread(code, sizeof (unsigned short), 1, file);
+        if(!readCount){
+            fprintf(stderr, "Unexpected EOF.\n");
+            exit(1);
+        }
+        if (code[0] > MAXN) {
+            fprintf(stderr, "Constant N too small %d > %d \n", code[0], MAXN);
+            exit(1);
+        }
+        bufferSize = 1;
+        zeroCounter = 0;
+        while (zeroCounter < code[0]) {
+            readCount = fread(code + bufferSize, sizeof (unsigned short), 1, file);
+            if(!readCount){
+                fprintf(stderr, "Unexpected EOF.\n");
+                exit(1);
+            }
+            if (code[bufferSize] == 0) zeroCounter++;
+            bufferSize++;
+        }
+    }
+
+    *length = bufferSize;
+    return (1);
+
+
+}
+
+//====================== USAGE =======================
+
+void help(char *name) {
+    fprintf(stderr, "The program %s counts the number of planar graphs in a file.\n\n", name);
+    fprintf(stderr, "Usage\n=====\n");
+    fprintf(stderr, " %s\n\n", name);
+    fprintf(stderr, "\nThis program can handle graphs up to %d vertices. Recompile if you need larger\n", MAXN);
+    fprintf(stderr, "graphs.\n\n");
+    fprintf(stderr, "Valid options\n=============\n");
+    fprintf(stderr, "    -h, --help\n");
+    fprintf(stderr, "       Print this help and return.\n");
+}
+
+void usage(char *name) {
+    fprintf(stderr, "Usage: %s [options]\n", name);
+    fprintf(stderr, "For more information type: %s -h \n\n", name);
+}
+
+int main(int argc, char *argv[]) {
+
+    /*=========== commandline parsing ===========*/
+
+    int c;
+    char *name = argv[0];
+    static struct option long_options[] = {
+        {"help", no_argument, NULL, 'h'}
+    };
+    int option_index = 0;
+
+    while ((c = getopt_long(argc, argv, "h", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 0:
+                break;
+            case 'h':
+                help(name);
+                return EXIT_SUCCESS;
+            case '?':
+                usage(name);
+                return EXIT_FAILURE;
+            default:
+                fprintf(stderr, "Illegal option %c.\n", c);
+                usage(name);
+                return EXIT_FAILURE;
+        }
+    }
+
+    /*=========== read planar graphs ===========*/
+
+    unsigned short code[MAXCODELENGTH];
+    int length;
+    while (readPlanarCode(code, &length, stdin)) {
+        numberOfGraphs++;
+    }
+    fprintf(stdout, "%d\n", numberOfGraphs);
+}
