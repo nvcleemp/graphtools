fc0b8e1b5e79d31b8777bf9fc1b98898a568f287
nvcleemp <nico.vancleemput@gmail.com>
2013-03-11 09:10:56 +0100

    Fixed bug: checked wrong letter for command-line option

--- statsPlanar.c
+++ statsPlanar.c
@@ -543,7 +543,7 @@ int main(int argc, char *argv[]) {
             case 'h':
                 help(name);
                 return EXIT_SUCCESS;
-            case 't':
+            case 's':
                 includeSummary = TRUE;
                 break;
             case 'f':
