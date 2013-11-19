1a3987e4def7631f82caabdacec525c9238dd7d6
nvcleemp <nico.vancleemput@gmail.com>
2013-11-19 15:33:20 +0100

    Removed unneeded code

--- planar/dual_pl.c
+++ planar/dual_pl.c
@@ -47,11 +47,6 @@ typedef struct e /* The data type used for edges */ {
     int mark, index; /* two ints for temporary use;
                           Only access mark via the MARK macros. */
 
-    int left_facesize; /* size of the face in prev-direction of the edge.
-        		  Only used for -p option. */
-    char angle; /* angle between this edge and next edge;
-                   0: alpha, 1: beta, 2: gamma, 3: delta */
-    int allowedInFaceMatching;
 } EDGE;
 
 EDGE *firstedge[MAXN]; /* pointer to arbitrary edge out of vertex i. */
@@ -431,7 +426,6 @@ void decodePlanarCode(unsigned short* code) {
         edges[edgeCounter].start = i;
         edges[edgeCounter].end = code[codePosition] - 1;
         edges[edgeCounter].next = edges + edgeCounter + 1;
-        edges[edgeCounter].allowedInFaceMatching = TRUE;
         if (code[codePosition] - 1 < i) {
             inverse = findEdge(code[codePosition] - 1, i);
             edges[edgeCounter].inverse = inverse;
@@ -450,7 +444,6 @@ void decodePlanarCode(unsigned short* code) {
             edges[edgeCounter].end = code[codePosition] - 1;
             edges[edgeCounter].prev = edges + edgeCounter - 1;
             edges[edgeCounter].next = edges + edgeCounter + 1;
-            edges[edgeCounter].allowedInFaceMatching = TRUE;
             if (code[codePosition] - 1 < i) {
                 inverse = findEdge(code[codePosition] - 1, i);
                 edges[edgeCounter].inverse = inverse;
